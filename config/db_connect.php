e43554f1cf9e699a40d427147cfd12b219c63ed5
Haider <Alihaider00760@gmail.com>
2020-03-22 22:19:40 +0500

    add comment to db_connect file

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 	// connect to the database
 	$conn = mysqli_connect('localhost', 'haider', 'test1234', 'ninja_pizza');
@@ -7,5 +7,5 @@
 	if(!$conn){
 		echo 'Connection error: '. mysqli_connect_error();
 	}
-
+//go show message to the user what is the error for not connecting to the database.
 ?>
